a9df75b3b7a616349aae1af7e6ba8d7a56480571
Ella Hamm <maryellahamm@gmail.com>
2022-04-24 21:49:35 -0400

    json script

--- scripts/main.js
+++ scripts/main.js
@@ -2,17 +2,25 @@ if (new Date().getHours() > 18) {
   //alert(new Date().getHours());
   const html = document.querySelector('html');
   html.style.backgroundColor = 'black';
-  html.style.color = 'white';
+  html.style.color = 'grey';
 }
-
+//logo header
 if (new Date().getHours() > 18) {
-  const wrapperBody = document.getElementsByClassName('wrapper');
-  wrapperBody[0].style.backgroundColor = 'black';
-  wrapperBody[0].style.color = 'white';
+  const logo = document.getElementsByClassName('logo');
+logo[0].style.backgroundColor = 'black';
 }
-
+//nav background
 if (new Date().getHours() > 18) {
-  const secondaryBody = document.getElementById('secondary');
-  secondaryBody.style.backgroundColor = 'black';
-  secondaryBody.style.color = 'white';
+  const nav__links = document.getElementsByClassName('nav__links');
+nav__links[0].style.backgroundColor = 'black';
+}
+if (new Date().getHours() > 18) {
+  const about = document.getElementsBy('about');
+  about.style.backgroundColor = 'black';
+  about.style.color = 'black';
+}
+//footer
+if (new Date().getHours() > 18) {
+  const footer = document.getElementsByClassName('footer');
+  footer[0].style.backgroundColor = 'black';
 }
